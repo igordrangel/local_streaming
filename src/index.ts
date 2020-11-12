856c1b714b2cad0ab65e00be22cd664c22bd3fa0
Igor <igor.rangel@ciadocredito.net.br>
2020-11-11 21:44:24 -0300

    commit

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ import { DB_CONNECTION_NAME } from "./factory/db-connection.factory";
 const customExpress = require("./config/custom-express");
 const app: Express = customExpress();
 
-const server = app.listen(80, async () => {
+const server = app.listen(3000, async () => {
 	const connectionManager = getConnectionManager();
 	const connectionOptions = await getConnectionOptions(DB_CONNECTION_NAME);
 	await connectionManager.create(connectionOptions).connect();
