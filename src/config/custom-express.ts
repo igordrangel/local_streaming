2fa7030520be5c922182e672bd6a9a8bfab24b90
Igor <igor.rangel@ciadocredito.net.br>
2020-11-29 00:41:10 -0300

    commit

--- src/config/custom-express.ts
+++ src/config/custom-express.ts
@@ -12,7 +12,7 @@ module.exports = () => {
 	app.use(express.json({limit: '100gb'}));
 	app.use(bodyParser.json());
 	app.use(cors({origin: "*"}));
-	consign({extensions: ['.ts']}).include('src/controller').into(app);
+	consign({extensions: ['.js']}).include('dist/controller').into(app);
 	app.use(timeout('1800s'));
 	return app;
 };
