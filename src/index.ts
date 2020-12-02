283412d3634925a154ee0aa11c3db7925794291d
Igor <igor.rangel@ciadocredito.net.br>
2020-12-02 14:42:25 -0300

    commit

--- src/index.ts
+++ src/index.ts
@@ -3,17 +3,30 @@ import { createConnection } from "typeorm";
 import { Express } from "express";
 import * as path from "path";
 import * as fs from "fs";
+import * as https from "https";
+import * as http from "http";
 
 createConnection().then(async connection => {
 	const customExpress = require("./config/custom-express");
 	const api: Express = customExpress();
-	const server = api.listen(3000, async () => {
-		const dirVideosPath = path.join(__dirname, '../_arquivos')
-		if (!fs.existsSync(dirVideosPath)) {
-			fs.mkdirSync(dirVideosPath);
-		}
-		console.log("Servidor LocalStreaming iniciado na porta 3000.");
-		console.log("Abra http://localhost:3000 para visualizar a documentação.");
+	
+	const dirVideosPath = path.join(__dirname, '../_arquivos')
+	if (!fs.existsSync(dirVideosPath)) {
+		fs.mkdirSync(dirVideosPath);
+	}
+	
+	const httpServer = http.createServer(api).listen(3001, async () => {
+		console.log("Servidor LocalStreaming iniciado na porta 3001.");
+		console.log("Abra http://localhost:3001 para visualizar a documentação.");
 	});
-	server.setTimeout(10800 * 1000);
+	httpServer.setTimeout(10800 * 1000);
+	
+	const httpsServer = https.createServer({
+		key: '',
+		cert: ''
+	}, api).listen(3000, async () => {
+		console.log("Servidor LocalStreaming iniciado na porta 3000.");
+		console.log("Abra https://localhost:3000 para visualizar a documentação.");
+	});
+	httpsServer.setTimeout(10800 * 1000);
 }).catch(error => console.log(error));
