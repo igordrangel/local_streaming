7faa35d098e1e7beec745f2137c7e29d1807e32f
Igor <igor.rangel@ciadocredito.net.br>
2020-12-02 14:46:18 -0300

    commit

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,6 @@ import { createConnection } from "typeorm";
 import { Express } from "express";
 import * as path from "path";
 import * as fs from "fs";
-import * as https from "https";
-import * as http from "http";
 
 createConnection().then(async connection => {
 	const customExpress = require("./config/custom-express");
@@ -15,18 +13,9 @@ createConnection().then(async connection => {
 		fs.mkdirSync(dirVideosPath);
 	}
 	
-	const httpServer = http.createServer(api).listen(3001, async () => {
-		console.log("Servidor LocalStreaming iniciado na porta 3001.");
-		console.log("Abra http://localhost:3001 para visualizar a documentação.");
-	});
-	httpServer.setTimeout(10800 * 1000);
-	
-	const httpsServer = https.createServer({
-		key: '',
-		cert: ''
-	}, api).listen(3000, async () => {
+	const server = api.listen(3000, async () => {
 		console.log("Servidor LocalStreaming iniciado na porta 3000.");
-		console.log("Abra https://localhost:3000 para visualizar a documentação.");
+		console.log("Abra http://localhost:3000 para visualizar a documentação.");
 	});
-	httpsServer.setTimeout(10800 * 1000);
+	server.setTimeout(10800 * 1000);
 }).catch(error => console.log(error));
