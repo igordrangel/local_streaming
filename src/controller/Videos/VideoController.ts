5c3dd14b4ab8deea641f7342cdbe2c70ede67afc
Igor <igor.rangel@ciadocredito.net.br>
2020-12-08 23:25:32 -0300

    commit

--- src/controller/Videos/VideoController.ts
+++ src/controller/Videos/VideoController.ts
@@ -91,7 +91,7 @@ module.exports = (api: Express) => {
 				res.set({
 					'Content-Type': 'video/mp4'
 				});
-				const video = fs.readFileSync(file);
+				const video = fs.readFileSync(file).toString('utf8');
 				res.status(200).send(video);
 			});
 		}
