4fb4996b7cf70ef672275fcbfdbb62eef18c6c27
Igor Rangel <igordrangel@gmail.com>
2021-02-24 12:25:47 -0300

    commit

--- src/repository/Video/VideoArquivoRepository.ts
+++ src/repository/Video/VideoArquivoRepository.ts
@@ -94,7 +94,10 @@ export default class VideoArquivoRepository extends Repository<VideoArquivo> {
 					
 					const currentPath = path.join(__dirname, `../../../_arquivos/${dirname}/${filename}`);
 					const newPath = path.join(__dirname, `../../../_arquivos/${dirname}/${newName}`);
-					if (ext !== 'mp4') {
+					if (ext === 'avi') {
+						await execSync(`ffmpeg -i "${currentPath}" -c:v copy -c:a copy -y "${newPath}"`);
+						await this.removeFile(dirname, filename);
+					} else if (ext !== 'mp4' && ext !== 'avi') {
 						await execSync(`ffmpeg -i "${currentPath}" -vcodec copy -acodec aac "${newPath}"`);
 						await this.removeFile(dirname, filename);
 					}
